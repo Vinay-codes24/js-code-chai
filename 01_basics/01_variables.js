b8d9aa856c299221d98317645ba408b3bf87cce4
Vinay-codes24 <bk2616725@gmail.com>
2025-06-16 13:35:49 +0000

    save new changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14453
+let accountEmail = "vinceleslawson@gmsil.com"
+var accountPassword = "12345"
+accountCity = "delhi"
+// var was not used bcz of bug in scope dont use it
+// accountId = 2nd
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all js as newer version
+
+// alert("hello") we r using node js not browser
+
+
+console.log(3 + 3)
+console.log("vinay")
+
+let name = "vinay" // string data type
+let age = "14" // number 2 ^ 53
