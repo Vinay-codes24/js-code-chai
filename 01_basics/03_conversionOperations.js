50f7b870a525be82effa3014d4cd273360fbe34e
Vinay-codes24 <bk2616725@gmail.com>
2025-06-17 04:29:30 +0000

    save

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -23,3 +23,30 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(someNumber);
+
+// ****************Operations ****************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+// -3
+
+let str1 = "hellow"
+let str2 = " vinay"
+
+let str3 = str1 + str2 ; // minus nhi hota
+
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 +"2");  // 32 jismei convert hoga depends on 1st input
+
+console.log(true); // true
+console.log(+true) // 1 increment nhi hua
+console.log(+"");  // 0
+
+let num1 , num2 , num3 
+
+num1 = num2 = num3 = 2+2 // this is not good way
+
+let gameCounter = 100
+gameCounter++ // 101
+ ++gameCounter // 101
---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,11 @@
+console.log("2" > 1); // true
+console.log("02" > 1);  // true
+
+console.log(null > 0);  // false
+console.log(null == 0);  // false
+console.log(null >= 0);  // true
+
+/* comapriosns 
+convert null to a number treating it as 0 .. equality doesnt == */
+
+console.log(undefined == 0); // true with any operations
