0b0c1578dbcabf8d29e1fcdfe69a9cbb33cdc5d3
Vinay-codes24 <bk2616725@gmail.com>
2025-06-17 07:38:48 +0000

    save

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,6 +1,34 @@
-const score = 400
+ const score = 400
+// console.log(score);
 
 const balance = new Number(100)
-console.log(balance); 
+// console.log(balance);
 
-console.log(balance.toString().length);
+// console.log(balance.toString().length);  wont round off
+// console.log(balance.toFixed(1));    
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4)); will round off
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); comma dega
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));   minus value postive
+// console.log(Math.round(4.6)); 
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
