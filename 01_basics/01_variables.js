fbe73f0146be884c093f855c82551c7e40e291be
Vinay-codes24 <bk2616725@gmail.com>
2025-06-17 04:03:07 +0000

    push

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,8 +4,20 @@ var accountPassword = "12345"
 accountCity = "delhi"
 // var was not used bcz of bug in scope dont use it
 // accountId = 2nd
+// big int
+// boolean ==> ture/false
+//null =. stand alone value
+//undefined => 
+// symbol => unique
 let accountState;
 
 console.log(accountId);
 
 console.table([accountId , accountEmail , accountCity]);
+
+// object
+console.log(typeof accountId);
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
+
+
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score) // string ko bhi uske kr skte
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+    //NaN
+ // "33" => 33
+ // "33abc" => NaN
+ // true => 1 and fasle => 0
+
+ let isLoggedIn = 1
+
+ let booleanIsLoogedIn = Boolean(isLoogedIn)
+console.log(booleanIsLoogedIn);
+
+// 1 true and 0 flase
+// "" false and "some value" true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(someNumber);
