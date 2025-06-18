ac56de3dbc97021b9eb3021115876de16e1eb50e
Vinay-codes24 <bk2616725@gmail.com>
2025-06-18 13:00:46 +0000

    continue

---
+++ 04_control_flow/one.js
@@ -0,0 +1,39 @@
+// if
+
+// if (true){
+// // true hai toh execute wrna flase toh nhi
+// }
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if(temperature < 50){
+// console.log(`less than 50`);
+
+// } else{
+//     console.log(`temp is greater than 50`);
+    
+// }
+
+
+// === even checks type !== checks neg
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"),
+
+// console.log("test2"); // immature code
+
+if (balance > 500) {
+    console.log("less than 500");
+    
+} else if (balance < 900){
+    console.log(`kuch bhi yaar`);
+    
+}
+
+const userLoggedIn = true
+const debitCard = true
+if (userLoggedIn && debitCard){
+    console.log(`yyou are allowed to purchase`);
+    
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,45 @@
+// switch ( key ) {
+// case value:
+
+// break;
+
+// default:
+//     break;
+
+// }
+
+const month = 3
+switch  (month ) {
+case  1:    
+console.log("january");
+break;
+
+case  2:    
+console.log("febaury");
+break;
+
+case  3:    
+console.log("april");
+break;
+
+default:
+    console.log("feault case match");
+    
+    break;
+// break tod deta hai control flow ko
+}
+
+// empty string is considered false
+// empty array is conisdered true
+
+/* falsy values
+
+false, 0 , -0 , BigInt 0n , "" , null , undefined , NaN
+
+other all are turthy values 
+
+some truth values suprises are 
+"0" , 'false' , " " , {} , {} , function(){} 
+*/
+const emptyObj
+
